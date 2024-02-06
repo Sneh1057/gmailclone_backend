9843bb32c1331bd2a3e59fbb158f74694288f2cd
sneh <kumarayush18960@gmail.com>
2024-02-06 13:16:13 +0530

    initial commit

--- database/db.js
+++ database/db.js
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
 
-const DATABASE = process.env.DATABASE;
+
 
 const connection = ()=>{
-    const DB_URI = `${DATABASE}`;
+    
+    const DB_URI = `mongodb://user2001:Sneh1050@ac-hpvwoda-shard-00-00.jkkf5yq.mongodb.net:27017,ac-hpvwoda-shard-00-01.jkkf5yq.mongodb.net:27017,ac-hpvwoda-shard-00-02.jkkf5yq.mongodb.net:27017/?ssl=true&replicaSet=atlas-69orwu-shard-0&authSource=admin&retryWrites=true&w=majority`;
        try {  mongoose.connect(DB_URI,{useNewUrlParser:true});
          console.log("database connected successfully");
+         
     }catch(error){
         console.log('Error while connecting with the database',error.message);
     }
